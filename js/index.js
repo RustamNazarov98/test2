9c0f0a4d79945f786c0ff27833e9caffe625d4d4
RustamNazarov98 <ruuus98@mail.ru>
2021-02-24 13:53:14 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+// coбытия
+// document.addEventListener('keydown', function(event) {
+//   if (event.code == 'KeyZ') {
+//     alert('Отменить!')
+//   }
+// });
+
+// let input = document.querySelector('input');
+// console.log(input.value);
+// dom
+document.body.style.background = 'red'; 
+setTimeout(() => document.body.style.background = '', 3000);
